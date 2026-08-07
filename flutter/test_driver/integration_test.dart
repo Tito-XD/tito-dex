99ca1138486778b84058b0b27c4ef68b3e6ae666
Tito <titow.xd@outlook.com>
2026-08-07 09:21:39 +0800

    test: emulator integration smoke (settings/media manager, dex back viewer)

---
+++ flutter/test_driver/integration_test.dart
@@ -0,0 +1,12 @@
+import 'dart:io';
+
+import 'package:integration_test/integration_test_driver_extended.dart';
+
+Future<void> main() => integrationDriver(
+  onScreenshot: (name, bytes, [args]) async {
+    final file = File('build/integration_test/$name.png');
+    file.parent.createSync(recursive: true);
+    file.writeAsBytesSync(bytes);
+    return true;
+  },
+);
